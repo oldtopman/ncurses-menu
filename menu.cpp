785447fd119a8afb9cce2fe5dcc8d69303e6c89d
Old Man <oldtopman@gmail.com>
2012-08-07 19:03:33 -0600

    Fixed bug causing odd text placement.

--- menu.cpp
+++ menu.cpp
@@ -2,9 +2,8 @@
 
 This is a menu-generating tool designedto be used with ncurses.
 The arguments to be passed to make are:
-Menu.make("title1","title2","title3","title4","title5",intEscape, intx, inty, intOptions);
-If intEscape is 0, then you have to choose an option.
-If intEscape is more than 0, you will be able to close the menu taking no action.
+Menu.make("title1","title2","title3","title4","title5",intWidth, intx, inty, intOptions);
+intWidth controls the width of the screen
 intx is the location of the upper left corner on the X axis.
 inty is the location of the upper left corner on the Y axis.
 intOptions controls the number of menu options.
@@ -23,7 +22,7 @@ Menu option selected, or -1 on error
 #include <stdlib.h>
 #include "menu.h"
 
-int Menu::make(const char* title1, const char* title2, const char* title3, const char* title4, const char* title5,int intEscape,int intx,int inty,int intOptions){
+int Menu::make(const char* title1, const char* title2, const char* title3, const char* title4, const char* title5,int intWidth,int intx,int inty,int intOptions){
 
     if (intOptions <= 0){
         return -1; //Return an error if invalid number of options selected
@@ -34,44 +33,44 @@ int Menu::make(const char* title1, const char* title2, const char* title3, const
     menuWindow = newwin(menuHeight, intWidth, inty, intx); //Create the window
     wborder(menuWindow, charSide, charSide, charTop, charTop, charCorner, charCorner, charCorner, charCorner); //Put the border on
     keypad(menuWindow, TRUE); //Init options for the screen
+	curs_set(0);
 
-    mvwprintw(menuWindow, inty+intActive, intx+intWidth-3, "<="); // Print the starting arrow
+    mvwprintw(menuWindow, intActive, intWidth-3, "<="); // Print the starting arrow
+
+    intArea = intWidth*menuHeight; //Calculate total area for cleanup program
 
     //Print out the titles
     if (intOptions >= 1){
-        mvwprintw(menuWindow, inty+intCounter, intx+1, title1);
+        mvwprintw(menuWindow, intCounter, 1, title1);
         intCounter++;
     }
 
     if (intOptions >= 2){
-        mvwprintw(menuWindow, inty+intCounter, intx+1, title2);
+        mvwprintw(menuWindow, intCounter, 1, title2);
         intCounter++;
     }
 
     if (intOptions >= 3){
-        mvwprintw(menuWindow, inty+intCounter, intx+1, title3);
+        mvwprintw(menuWindow, intCounter, 1, title3);
         intCounter++;
     }
 
     if (intOptions >= 4){
-        mvwprintw(menuWindow, inty+intCounter, intx+1, title4);
+        mvwprintw(menuWindow, intCounter, 1, title4);
         intCounter++;
     }
 
     if (intOptions >= 5){
-        mvwprintw(menuWindow, inty+intCounter, intx+1, title5);
+        mvwprintw(menuWindow, intCounter, 1, title5);
         intCounter++;
     }
 
-
     wrefresh(menuWindow); //Draw the screen
+
+    //while(true); //Test the initial drawings
 
     while(intControl != 1){
         longChar = wgetch(menuWindow); //Get the keypress
-
-        if (intEscape == 1 && longChar == 27){ //If intEscape is set, add escape option with escape key
-            intControl = 1;
-        }
 
         if (longChar == KEY_UP){
             intLastActive = intActive; //Preserve last location for clearing it
@@ -87,8 +86,8 @@ int Menu::make(const char* title1, const char* title2, const char* title3, const
             }
         }
 
-        mvwprintw(menuWindow, inty+intLastActive, intx+intWidth-3, "  "); //Clear out the old cursor
-        mvwprintw(menuWindow, inty+intActive, intx+intWidth-3, "<="); //Draw and refresh the cursor
+        mvwprintw(menuWindow, intLastActive, intWidth-3, "  "); //Clear out the old cursor
+        mvwprintw(menuWindow, intActive, intWidth-3, "<="); //Draw and refresh the cursor
         wrefresh(menuWindow);
 
         if (longChar == 10){ //Here's the code to return the correct number
@@ -105,6 +104,11 @@ int Menu::make(const char* title1, const char* title2, const char* title3, const
 void Menu::clean(){
     box(menuWindow, ' ', ' ');
     wborder(menuWindow, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+    mvwprintw(menuWindow, 0, 0, " ");
+    while (intArea > 0) {
+        wprintw(menuWindow, " ");
+        intArea--;
+    }
     wrefresh(menuWindow);
     delwin(menuWindow);
 }
